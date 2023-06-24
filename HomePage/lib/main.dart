b1b3565ad19f52132598b1c21d236ff880022431
miguelpoot <93948699+miguelpoot@users.noreply.github.com>
2023-06-24 15:21:55 -0600

    login-registro
    elaboración de login y de registro

--- HomePage/lib/main.dart
+++ HomePage/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:justifycations_app/pages/home_page.dart';
+import 'package:justifications_app/pages/login_page.dart';
+import 'package:justifications_app/pages/registro_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,10 +9,17 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      initialRoute: 'home',
+      routes: {
+        'home' : (BuildContext context) => HomePage(),
+        'login' : (BuildContext context) => LoginPage(),
+        'registro' : (BuildContext context) => RegistroPage(),
+
+        
+      },
     );
   }
 }
---
+++ HomePage/lib/pages/login_page.dart
@@ -0,0 +1,136 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:justifications/widgets/appbarLR.dart';
+
+import 'registro_page.dart';
+
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: colorR(),
+        body: cuerpo(context)
+        
+        );
+  }
+}
+
+Widget cuerpo( BuildContext context ) {
+  return Container(
+    padding: EdgeInsets.symmetric(vertical: 50),
+    child: SingleChildScrollView(
+      child: Container(
+        decoration: BoxDecoration(
+        ),
+        child: Center(
+          
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            
+            children: [
+              
+              nombre(),
+              _username(context),
+              _password(context),
+              _crearButton(context),
+              _crearNCuenta(context)
+            ],
+          )
+        ),
+      ),
+    ),
+  );
+}
+
+
+
+Widget nombre(){
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+    child: Image(image: AssetImage('assets/LogoCbtis72_1.png')));
+}
+
+ Widget _username(BuildContext context) {
+
+
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 3, horizontal: 20),
+      child: TextField(
+            decoration: InputDecoration(
+                hintText: "Username",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(18),
+                    borderSide: BorderSide.none),
+                fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+                filled: true,
+                prefixIcon: Icon(Icons.person, color: Colors.red.shade900)),
+          ),
+    );
+
+
+  }
+
+  Widget _password(BuildContext context){
+    return  Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 3),
+      child: TextField(
+            decoration: InputDecoration(
+              hintText: "Password",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                  borderSide: BorderSide.none),
+              fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+              filled: true,
+              prefixIcon: Icon(Icons.lock_sharp, color: Colors.red.shade900),
+            ),
+            obscureText: true,
+          ),
+    );
+  }
+  Widget _crearNCuenta(BuildContext context){
+     return TextButton(
+              child:Text('Crear una nueva cuenta', style: TextStyle(color: Colors.red.shade900),) ,
+              
+              onPressed: () {
+
+                  final route = MaterialPageRoute(
+            builder: (context){
+              return RegistroPage();
+            }
+            );
+
+            Navigator.push(context, route);
+                
+              },
+              );
+  }
+
+
+
+  Widget _crearButton(BuildContext context){
+    return  Container(
+      padding: EdgeInsets.all(22),
+      child: ElevatedButton(
+        
+            onPressed: () {},
+            child: Text(
+              "Login",
+              style: TextStyle(fontSize: 20, ),
+              
+            ),
+            style: ElevatedButton.styleFrom(
+              
+              shape: StadiumBorder(),
+              padding: EdgeInsets.symmetric(vertical:20, horizontal: 60),
+              backgroundColor: Colors.red.shade900,
+              
+              
+            ),
+           
+          ),
+    );
+  }
